604df4da805153308d85cebb90b123774d32ba90
Miraneek <surykata05@gmail.com>
2024-03-11 16:27:09 +0100

    trying to add a button, not done

--- src/components/navbar/ColorButton/ColorButton.js
+++ src/components/navbar/ColorButton/ColorButton.js
@@ -1,5 +1,6 @@
 "use client"
-import { useState, useEffect } from 'react';
+import styles from "./colorButton.module.css"
+import {useState, useEffect} from 'react';
 
 export default function ColorButton() {
 
@@ -26,14 +27,43 @@ export default function ColorButton() {
     }, [darkMode]);
 
     return (
-        <>
-            <button onClick={toggleColorScheme}>
-                {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
-            </button>
-            <h2>
-                {darkMode ? 'Dark Mode' : 'Light Mode'}
-            </h2>
-        </>
+        <button onClick={toggleColorScheme} className={styles.button}>
+            {darkMode ? (
+                <svg width="30" height="30">
+                    <circle cx="15" cy="15" r="6" fill="currentColor"/>
 
+                    <line
+                        id="ray"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        x1="15"
+                        y1="1"
+                        x2="15"
+                        y2="4"
+                    ></line>
+
+                    <use href="#ray" transform="rotate(45 15 15)"/>
+                    <use href="#ray" transform="rotate(90 15 15)"/>
+                    <use href="#ray" transform="rotate(135 15 15)"/>
+                    <use href="#ray" transform="rotate(180 15 15)"/>
+                    <use href="#ray" transform="rotate(225 15 15)"/>
+                    <use href="#ray" transform="rotate(270 15 15)"/>
+                    <use href="#ray" transform="rotate(315 15 15)"/>
+                </svg>
+            ) : (
+                <svg width="30" height="30">
+                    <path
+                        fill="currentColor"
+                        d="
+          M 23, 5
+          A 12 12 0 1 0 23, 25
+          A 12 12 0 0 1 23, 5"
+                    />
+                </svg>
+            )}
+
+
+        </button>
     );
 };
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,14 +4,15 @@ import RightSide from "@/components/navbar/RightSide/RightSide";
 import ZpetnaVazba from "@/components/navbar/zpetna vazba/ZpetnaVazba";
 import ColorButton from "@/components/navbar/ColorButton/ColorButton";
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         <>
             <header className={styles.header}>
                 <ZpetnaVazba/>
                 <nav className={styles.nav}>
                     <LogoSection/>
                     <RightSide/>
+
                 </nav>
                 <ColorButton/>
             </header>
