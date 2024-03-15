bef12f126b4b925050fe0363770788f5a49481f4
Miraneek <surykata05@gmail.com>
2024-03-15 18:35:18 +0100

    Animations🏄‍♂️

--- src/components/navbar/RightSide/RightSide.js
+++ src/components/navbar/RightSide/RightSide.js
@@ -1,28 +1,28 @@
-"use client"
-import styles from "./rightside.module.css"
+"use client";
+import React, { useState } from "react";
+import styles from "./rightside.module.css";
 import LinksSection from "@/components/navbar/RightSide/LinksSection/LinksSection";
 import SearchSection from "@/components/navbar/RightSide/SearchSection/SearchSection";
-import {useState} from "react";
 import HamburgerMenu from "@/components/navbar/RightSide/HamburgerButton/HamburgerButton";
 
-
-export default function RightSide(){
-
-    let [isOpen, setIsOpen] = useState(false);
+export default function RightSide() {
+    const [isOpen, setIsOpen] = useState(false);
 
     function handleToggle() {
-        console.log(isOpen)
         setIsOpen(!isOpen);
     }
 
-    return(
+    return (
         <div>
-            <HamburgerMenu handleToggle={handleToggle}/>
-            <section className={styles.rightSide + " " + `${styles.mobileMenu} ${isOpen ? styles.open : ''}`}>
-                <LinksSection/>
-                <SearchSection/>
+            <HamburgerMenu handleToggle={handleToggle} />
+            <section
+                className={`${styles.rightSide} ${styles.mobileMenu} ${
+                    isOpen ? styles.open : " "
+                }`}
+            >
+                <LinksSection />
+                <SearchSection />
             </section>
         </div>
-
-    )
+    );
 }
