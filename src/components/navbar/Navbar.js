d273f1a612b4becd3706031caed4f9136e703afb
Miraneek <surykata05@gmail.com>
2024-03-15 18:16:39 +0100

    Animations🏄‍♂️

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,11 +2,12 @@ import LogoSection from "@/components/navbar/LogoSection/LogoSection";
 import styles from "./navbar.module.css";
 import RightSide from "@/components/navbar/RightSide/RightSide";
 import ZpetnaVazba from "@/components/navbar/zpetna vazba/ZpetnaVazba";
+import 'animate.css';
 
 export default function Navbar() {
     return (
         <>
-            <header className={styles.header}>
+            <header className={styles.header + " animate__animated animate__fadeInDown"}>
                 <ZpetnaVazba/>
                 <nav className={styles.nav}>
                     <LogoSection/>
--- src/components/otherComponents/Hero/Hero.js
+++ src/components/otherComponents/Hero/Hero.js
@@ -1,13 +1,14 @@
 import styles from './hero.module.css'
+import Image from "next/image";
+import 'animate.css';
 export default function Hero() {
 
     return (
         <section className={styles.section}>
-            <h1>
+            <h1 className={"animate__animated animate__fadeInLeftBig"}>
                 <strong>Učíme</strong> mladou generaci <strong><span>finanční</span> gramotnost</strong>.
             </h1>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={"/other/landingPage/planetka.png"} alt="planeta img"/>
+            <Image src={"/other/landingPage/planetka.png"} alt="planeta img" className={"animate__animated animate__fadeInRight"} width={1000} height={1000}/>
         </section>
     )
 }
