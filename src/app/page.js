e58eca863b1872c0d28ba769d8657771f8af0b8f
filma <fil.masilko@gmail.com>
2024-03-20 12:23:11 +0100

    🤗

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <>
             <Hero/>
-            <Carte Name={"Jak vyplnit ružovej papír"} Image= {"/CarteObrazky/ruzovejpapir.png"} Url={"https://forms.gle/LTxMtpbreAKNXdKj9"} Text={"K vyplnění růžového daňového přiznání pro fyzické osoby v České  republice je nezbytné poskytnout přesné osobní a finanční informace.\n" +
+            <Carte Name={"Jak vyplnit ružovej papír?"} Image= {"/CarteObrazky/ruzovejpapir.png"} Url={"./clanky/Co-je-to-ruzovy-papir"} Text={"K vyplnění růžového daňového přiznání pro fyzické osoby v České  republice je nezbytné poskytnout přesné osobní a finanční informace.\n" +
                 "Detailně vykážte všechny zdroje příjmů a uplatněte příslušné výdaje a  slevy. Výsledná částka daně se spočítá na základě vašich celkových  příjmů a možných odpočtů."}/>
 
 
