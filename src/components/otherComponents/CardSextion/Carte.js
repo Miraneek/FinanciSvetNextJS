32af5a859db107c3bdcc137b43894bf4bb0b545a
Miraneek <surykata05@gmail.com>
2024-03-20 13:41:45 +0100

    cursor gone :((

--- src/components/otherComponents/CardSextion/Carte.js
+++ src/components/otherComponents/CardSextion/Carte.js
@@ -5,9 +5,9 @@ export default function Carte({Name, Image, Url}) {
 
     return (
         <Link href={Url} className={style.obalkarty}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img className={style.mainimage} src={Image} alt={Name}/>
                 <h1 className={style.nametext}>{Name}</h1>
-
         </Link>
     )
 }
