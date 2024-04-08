68a7ae19b746631f54838b4681bb93bb199c4d13
Miraneek <surykata05@gmail.com>
2024-04-08 14:57:23 +0200

    Tailwind

--- src/app/clanky/Jak-na-pohovor/page.js
+++ src/app/clanky/Jak-na-pohovor/page.js
@@ -1,10 +1,24 @@
+import TextWithImage from "@/components/PageBuilders/TextWithImage/TextWithImage";
+
 export default function Page() {
     return (
         <>
             <h1>jak na pohovor</h1>
-            <p>
+            <p className={"text-4xl"}>
                 umyju vlasy, přijdu, rizznu a ízíčko.
             </p>
+            <TextWithImage isOnLeft={false} nadpis={"jak na pohovor"} imgSRC={"/cojetoruzovipapirobrazky/manthinkin.png"} textPosition={"left"}>
+                <a href={"https://forms.gle/LTxMtpbreAKNXdKj9"}>odkaz zde</a>
+            </TextWithImage>
+            <TextWithImage isOnLeft={true} nadpis={"jak na pohovor"} imgSRC={"/cojetoruzovipapirobrazky/manthinkin.png"} textPosition={"left"}>
+                <a href={"https://forms.gle/LTxMtpbreAKNXdKj9"}>odkaz zde</a>
+            </TextWithImage>
+            <TextWithImage isOnLeft={false} nadpis={"jak na pohovor"} imgSRC={"/cojetoruzovipapirobrazky/manthinkin.png"} textPosition={"left"}>
+                <a href={"https://forms.gle/LTxMtpbreAKNXdKj9"}>odkaz zde</a>
+            </TextWithImage>
+            <TextWithImage isOnLeft={true} nadpis={"jak na pohovor"} imgSRC={"/cojetoruzovipapirobrazky/manthinkin.png"} textPosition={"left"}>
+                <a href={"https://forms.gle/LTxMtpbreAKNXdKj9"}>odkaz zde</a>
+            </TextWithImage>
         </>
     )
 }
