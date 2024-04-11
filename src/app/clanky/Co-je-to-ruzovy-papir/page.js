0791044d831422c256afd651dc814652ed2f6170
filma <fil.masilko@gmail.com>
2024-04-11 12:01:40 +0200

    oprava pravopisnejch chyb

--- src/app/clanky/Co-je-to-ruzovy-papir/page.js
+++ src/app/clanky/Co-je-to-ruzovy-papir/page.js
@@ -4,13 +4,13 @@ import TextWithImage from "@/components/PageBuilders/TextWithImage";
 export default function coJeToRuzovyPapir() {
     return (
         <>
-            <TextWithImage nadpis={"Co je to ruzovy papir?"} imgSRC={"/co-je-to-ruzovy-papir-orazky/manthinkin.png"} isLeft={true}>
+            <TextWithImage nadpis={"Co je to ruzovy papir?"} imgSRC={"/co-je-to-ruzovy-papir-obrazky/manthinkin.png"} isLeft={true}>
                 <Link href={"https://www.google.cz"}>Velmi cool link</Link>
             </TextWithImage>
-            <TextWithImage nadpis={"Co je to ruzovy papir?"} imgSRC={"/co-je-to-rzovy-papir-obrazky/manthinkin.png"} isLeft={false}>
+            <TextWithImage nadpis={"Co je to ruzovy papir?"} imgSRC={"/co-je-to-ruzovy-papir-obrazky/manthinkin.png"} isLeft={false}>
                 <Link href={"https://www.google.cz"}>Velmi cool link</Link>
             </TextWithImage>
-            <TextWithImage nadpis={"Co je to ruzovy papir?"} imgSRC={"/co-je-to-uzovy-papir-obrazky/manthinkin.png"} isLeft={true}>
+            <TextWithImage nadpis={"Co je to ruzovy papir?"} imgSRC={"/co-je-to-ruzovy-papir-obrazky/manthinkin.png"} isLeft={true}>
                 <Link href={"https://www.google.cz"}>Velmy cool link</Link>
             </TextWithImage>
         </>
