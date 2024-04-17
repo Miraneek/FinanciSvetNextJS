6180be6cc3c9b91d530ffe8d2e535b8a634daea2
Miraneek <surykata05@gmail.com>
2024-04-17 12:39:32 +0200

    empty clanek update

--- src/app/clanky/[emptyClanek]/page.js
+++ src/app/clanky/[emptyClanek]/page.js
@@ -1,11 +1,17 @@
-export default function Page({ params }) {
+import Link from "next/link";
+
+export default function Page({params}) {
     return (
         <>
-            <h1>{params.emptyClanek.split('-').join(' ')}</h1>
-            <p>
-                Tento článek buď neexistuje, nebo ještě nebyl napsán. <br/>
+            <h1 className={"text-4xl mb-2"}>{params.emptyClanek.split('-').join(' ')}</h1>
+            <p className={"text-[20px]"}>
+                Tento článek buď neexistuje, nebo ještě nebyl napsán.
                 Tato stránka je teprve prototyp a ne ještě plně fungující stránka.
             </p>
+            <h4 className={"text-4xl mt-4"}>
+                Jediný funkční článek je v této době tento
+            </h4>
+            <Link href={"./Co-je-to-ruzovy-papir"} className={"text-3xl mt-4 text-blue-500 hover:text-blue-700 font-bold transition-all delay-1s"}>Co je to růžový papír</Link>
         </>
     )
 }
